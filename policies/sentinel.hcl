c7f50d643c52d034922281bcb79b964551efdf2c
S <pallavi.e.s@capgemini.com>
2023-09-21 13:08:54 +0530

    First commit

---
+++ policies/sentinel.hcl
@@ -0,0 +1,20 @@
+module "tfplan-functions" {
+    source = "../common-functions/tfplan-functions/tfplan-functions.sentinel"
+}
+
+module "tfstate-functions" {
+    source = "../common-functions/tfstate-functions/tfstate-functions.sentinel"
+}
+
+module "tfconfig-functions" {
+    source = "../common-functions/tfconfig-functions/tfconfig-functions.sentinel"
+}
+
+module "azure-functions" {
+    source = "./azure-functions/azure-functions.sentinel"
+}
+
+policy "restrict-vm-size" {
+    source = "./restrict-vm-size.sentinel"
+    enforcement_level = "advisory"
+}
---
+++ vm.tf
@@ -0,0 +1,102 @@
+terraform {
+
+  cloud {
+    organization = "example-sentinel"
+
+    workspaces {
+      name = "learn-terraform-sentinel"
+    }
+  }
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "3.54.0"
+    }
+  }
+}
+
+  provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "RG" {
+  name     = "Sentinel-RG"
+  location = "CentralUS"
+}
+
+resource "azurerm_virtual_network" "vnet" {
+  name                = "Sentinel-vnet"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.RG.location
+  resource_group_name = azurerm_resource_group.RG.name
+}
+
+resource "azurerm_subnet" "snet" {
+  name                 = "Sentinel-snet"
+  resource_group_name  = azurerm_resource_group.RG.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.2.0/24"]
+}
+
+resource "azurerm_network_interface" "nic" {
+  name                = "Sentinel-nic"
+  location            = azurerm_resource_group.RG.location
+  resource_group_name = azurerm_resource_group.RG.name
+
+  ip_configuration {
+    name                          = "test"
+    subnet_id                     = azurerm_subnet.snet.id
+    private_ip_address_allocation = "Dynamic"
+  }
+}
+
+resource "azurerm_windows_virtual_machine" "win" {
+  name                = "win-machine"
+  resource_group_name = azurerm_resource_group.RG.name
+  location            = azurerm_resource_group.RG.location
+  size                = ""
+  admin_username      = "adminuser"
+  admin_password      = "P@$$w0rd1234!"
+  #computer_name       = "test-win"
+  network_interface_ids = [
+    azurerm_network_interface.nic.id,
+  ]
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "MicrosoftWindowsServer"
+    offer     = "WindowsServer"
+    sku       = "2016-Datacenter"
+    version   = "latest"
+  }
+}
+
+resource "azurerm_linux_virtual_machine" "lin" {
+  name              		  = "lin-machine"
+  resource_group_name		  = azurerm_resource_group.RG.name
+  location         	      = azurerm_resource_group.RG.location
+  size               		  = ""
+  admin_username     	          = "adminuser"
+  disable_password_authentication = false
+  admin_password                  = "P@$$w0rd1234!" 
+  network_interface_ids = [
+    azurerm_network_interface.nic.id,
+  ]
+
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "0001-com-ubuntu-server-focal"
+    sku       = "20_04-lts"
+    version   = "latest"
+  }
+}
