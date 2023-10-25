7c1e430b0c26961df5404d8dae10c079931a0b84
Pallavi Sajangadde <pallavisajangadde@gmail.com>
2023-10-25 18:32:05 +0530

    Update test-vmss.tf

--- test-vmss.tf
+++ test-vmss.tf
@@ -29,7 +29,7 @@ resource "azurerm_virtual_machine_scale_set_extension" "tlz_extension_linux" {
     
     #fileUris = ["https://github.com/pallavis3/sentinel/blob/792db2a1aa2839017f8e498606a559c87bad9b34/powershell-win2019.zip"],
    # commandToExecute = "powershell -NoProfile -windowstyle hidden -ExecutionPolicy Unrestricted -Command \"Invoke-WebRequest -Uri \"www.google.com\" -UseBasicParsing | Out-File -FilePath C:\\ansible.txt;.\\win-host.ps1 Pallavi ${data.azurerm_subscription.current.subscription_id} CentralUS VMSS  1 FScode vmsshost typevmss dev prod ${var.fapp_key}\"",
-   commandToExecute = "powershell -NoProfile -windowstyle hidden -ExecutionPolicy Unrestricted -Command \"Expand-Archive -Path sentinel/powershell-win2019.zip -DestinationPath C:\reference \""
+   commandToExecute = "powershell -NoProfile -windowstyle hidden -ExecutionPolicy Unrestricted -Command \"Expand-Archive -Path https://github.com/pallavis3/sentinel/blob/64f6a7450594018fd26a16f9b090e28f34da0e0c/powershell-win2019.zip -DestinationPath C:\reference \""
     managedIdentity = {}
     })
  #depends_on = [ azurerm_role_assignment.saroleforvmss ]
